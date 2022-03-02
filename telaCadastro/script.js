9626a1c673000cb7919b27a75b6959f05af6337d
DrigsGomes <rodrigo.vitor169@gmail.com>
2022-03-01 22:57:52 -0300

    Update.js

--- telaCadastro/script.js
+++ telaCadastro/script.js
@@ -1,14 +1,21 @@
 let nome = document.querySelector('#nome');
 let labelNome = document.querySelector('#labelNome');
+let validNome = false;
 
 let usuario = document.querySelector('#usuario');
 let labelUsuario = document.querySelector('#labelUsuario');
+let validUsuario = false;
+
 
 let senha = document.querySelector('#senha');
 let labelSenha = document.querySelector('#labelSenha');
+let validSenha = false;
+
 
 let confirmSenha = document.querySelector('#confirmSenha');
 let labelConfirmSenha = document.querySelector('#labelConfirmSenha');
+let validConfirmSenha = false;
+
 
 let verSenha = document.querySelector("#verSenha");
 let btnConfirm = document.querySelector("#verConfirmSenha");
@@ -20,12 +27,14 @@ nome.addEventListener('keyup', () => {
         labelNome.setAttribute('style', 'color:red')
         labelNome.innerHTML = 'Nome *Insirano no minimo 3 caracteres'
         nome.setAttribute('style', 'border-color: red')
+        validNome = false;
         
 
     } else{
         labelNome.setAttribute('style', 'color:green')
         labelNome.innerHTML= ('Nome')
         nome.setAttribute('style', 'border-color:green')
+        validNome = true;
         
 
     }
@@ -38,12 +47,14 @@ usuario.addEventListener('keyup', () => {
         labelUsuario.setAttribute('style', 'color:red')
         labelUsuario.innerHTML = 'Usuario *Insirano no minimo 5 caracteres'
         usuario.setAttribute('style', 'border-color: red')
+        validUsuario = false;
         
 
     } else{
         labelUsuario.setAttribute('style', 'color:green')
         labelUsuario.innerHTML= ('Usuario')
         usuario.setAttribute('style', 'border-color:green')
+        validUsuario = true;
         
 
     }
@@ -56,12 +67,14 @@ senha.addEventListener('keyup', () => {
         labelSenha.setAttribute('style', 'color:red')
         labelSenha.innerHTML = 'Senha *Insirano no minimo 6 caracteres'
         senha.setAttribute('style', 'border-color: red')
+        validSenha = false;
         
 
     } else{
         labelSenha.setAttribute('style', 'color:green')
         labelSenha.innerHTML= ('Senha')
         senha.setAttribute('style', 'border-color:green')
+        validSenha = true;
         
 
     }
@@ -70,25 +83,30 @@ senha.addEventListener('keyup', () => {
 
 confirmSenha.addEventListener('keyup', () => {
     if(senha.value != confirmSenha.value){
-        
-        labelConfirmSenha.setAttribute('style', 'color:red')
-        lablabelConfirmSenhaelSenha.innerHTML = 'Confirmar senha *As senhas não conferem'
-        confirmSenha.setAttribute('style', 'border-color: red')
-        
+      labelConfirmSenha.setAttribute('style', 'color: red')
+      labelConfirmSenha.innerHTML = 'Confirmar Senha *As senhas não conferem'
+      confirmSenha.setAttribute('style', 'border-color: red')
+      validConfirmSenha = false
+    } else {
+      labelConfirmSenha.setAttribute('style', 'color: green')
+      labelConfirmSenha.innerHTML = 'Confirmar Senha'
+      confirmSenha.setAttribute('style', 'border-color: green')
+      validConfirmSenha = true
 
-    } else{
-        labelConfirmSenha.setAttribute('style', 'color:green')
-        labelConfirmSenha.innerHTML= ('Confirmar senha')
-        confirmSenha.setAttribute('style', 'border-color:green')
-        
 
     }
-
-})
+    
+  })
+  
 
 
 
 function cadastrar (){
+    if(validNome && validUsuario && validSenha && validConfirmSenha){
+
+    }else{
+
+    }
 
 };
 
