df30025e8683eb0333397211a547189e423adb1d
DrigsGomes <rodrigo.vitor169@gmail.com>
2022-03-07 22:05:03 -0300

    Update script.js

--- telaCadastro/script.js
+++ telaCadastro/script.js
@@ -1,85 +1,89 @@
-let btn = document.querySelector('#verSenha')
-let btnConfirm = document.querySelector('#verConfirmSenha')
+let btn = document.querySelector('#verSenha');
+let btnConfirm = document.querySelector('#verConfirmSenha');
 
 
-let nome = document.querySelector('#nome')
-let labelNome = document.querySelector('#labelNome')
+let nome = document.querySelector('#nome');
+let labelNome = document.querySelector('#labelNome');
 let validNome = false
 
 let usuario = document.querySelector('#usuario')
-let labelUsuario = document.querySelector('#labelUsuario')
+let labelUsuario = document.querySelector('#labelUsuario');
 let validUsuario = false
 
-let senha = document.querySelector('#senha')
-let labelSenha = document.querySelector('#labelSenha')
+let senha = document.querySelector('#senha');
+let labelSenha = document.querySelector('#labelSenha');
 let validSenha = false
 
-let confirmSenha = document.querySelector('#confirmSenha')
-let labelConfirmSenha = document.querySelector('#labelConfirmSenha')
+let confirmSenha = document.querySelector('#confirmSenha');
+let labelConfirmSenha = document.querySelector('#labelConfirmSenha');
 let validConfirmSenha = false
 
-let msgError = document.querySelector('#msgError')
-let msgSuccess = document.querySelector('#msgSuccess')
+let msgError = document.querySelector('#msgError');
+let msgSuccess = document.querySelector('#msgSuccess');
 
+
+///Validando os campos de input
 nome.addEventListener('keyup', () => {
   if(nome.value.length <= 2){
-    labelNome.setAttribute('style', 'color: red')
+    labelNome.setAttribute('style', 'color: red');
     labelNome.innerHTML = 'Nome *Insira no minimo 3 caracteres'
-    nome.setAttribute('style', 'border-color: red')
+    nome.setAttribute('style', 'border-color: red');
     validNome = false
   } else {
-    labelNome.setAttribute('style', 'color: green')
+    labelNome.setAttribute('style', 'color: green');
     labelNome.innerHTML = 'Nome'
-    nome.setAttribute('style', 'border-color: green')
+    nome.setAttribute('style', 'border-color: green');
     validNome = true
   }
 })
 
 usuario.addEventListener('keyup', () => {
   if(usuario.value.length <= 4){
-    labelUsuario.setAttribute('style', 'color: red')
+    labelUsuario.setAttribute('style', 'color: red');
     labelUsuario.innerHTML = 'Usuário *Insira no minimo 5 caracteres'
-    usuario.setAttribute('style', 'border-color: red')
+    usuario.setAttribute('style', 'border-color: red');
     validUsuario = false
   } else {
-    labelUsuario.setAttribute('style', 'color: green')
+    labelUsuario.setAttribute('style', 'color: green');
     labelUsuario.innerHTML = 'Usuário'
-    usuario.setAttribute('style', 'border-color: green')
+    usuario.setAttribute('style', 'border-color: green');
     validUsuario = true
   }
 })
 
 senha.addEventListener('keyup', () => {
   if(senha.value.length <= 5){
-    labelSenha.setAttribute('style', 'color: red')
+    labelSenha.setAttribute('style', 'color: red');
     labelSenha.innerHTML = 'Senha *Insira no minimo 6 caracteres'
-    senha.setAttribute('style', 'border-color: red')
+    senha.setAttribute('style', 'border-color: red');
     validSenha = false
   } else {
-    labelSenha.setAttribute('style', 'color: green')
+    labelSenha.setAttribute('style', 'color: green');
     labelSenha.innerHTML = 'Senha'
-    senha.setAttribute('style', 'border-color: green')
+    senha.setAttribute('style', 'border-color: green');
     validSenha = true
   }
 })
 
 confirmSenha.addEventListener('keyup', () => {
   if(senha.value != confirmSenha.value){
-    labelConfirmSenha.setAttribute('style', 'color: red')
+    labelConfirmSenha.setAttribute('style', 'color: red');
     labelConfirmSenha.innerHTML = 'Confirmar Senha *As senhas não conferem'
-    confirmSenha.setAttribute('style', 'border-color: red')
+    confirmSenha.setAttribute('style', 'border-color: red');
     validConfirmSenha = false
   } else {
-    labelConfirmSenha.setAttribute('style', 'color: green')
+    labelConfirmSenha.setAttribute('style', 'color: green');
     labelConfirmSenha.innerHTML = 'Confirmar Senha'
-    confirmSenha.setAttribute('style', 'border-color: green')
+    confirmSenha.setAttribute('style', 'border-color: green');
     validConfirmSenha = true
   }
 })
 
+/// Salvando os dados dos inputs no localStores 
+
 function cadastrar(){
   if(validNome && validUsuario && validSenha && validConfirmSenha){
-    let listaUser = JSON.parse(localStorage.getItem('listaUser') || '[]')
+    let listaUser = JSON.parse(localStorage.getItem('listaUser') || '[]');
     
     listaUser.push(
     {
@@ -94,29 +98,29 @@ function cadastrar(){
    
     msgSuccess.setAttribute('style', 'display: block')
     msgSuccess.innerHTML = '<strong>Cadastrando usuário...</strong>'
-    msgError.setAttribute('style', 'display: none')
+    msgError.setAttribute('style', 'display: none');
     msgError.innerHTML = ''
     
     setTimeout(()=>{
         window.location.href = 'http://127.0.0.1:5500/'
-    }, 3000)
+    }, 3000);
   
     
   } else {
-    msgError.setAttribute('style', 'display: block')
+    msgError.setAttribute('style', 'display: block');
     msgError.innerHTML = '<strong>Preencha todos os campos corretamente antes de cadastrar</strong>'
     msgSuccess.innerHTML = ''
-    msgSuccess.setAttribute('style', 'display: none')
+    msgSuccess.setAttribute('style', 'display: none');
   }
 }
 
 btn.addEventListener('click', ()=>{
-  let inputSenha = document.querySelector('#senha')
+  let inputSenha = document.querySelector('#senha');
   
   if(inputSenha.getAttribute('type') == 'password'){
-    inputSenha.setAttribute('type', 'text')
+    inputSenha.setAttribute('type', 'text');
   } else {
-    inputSenha.setAttribute('type', 'password')
+    inputSenha.setAttribute('type', 'password');
   }
 })
 
@@ -124,8 +128,8 @@ btnConfirm.addEventListener('click', ()=>{
   let inputConfirmSenha = document.querySelector('#confirmSenha')
   
   if(inputConfirmSenha.getAttribute('type') == 'password'){
-    inputConfirmSenha.setAttribute('type', 'text')
+    inputConfirmSenha.setAttribute('type', 'text');
   } else {
-    inputConfirmSenha.setAttribute('type', 'password')
+    inputConfirmSenha.setAttribute('type', 'password');
   }
 })
